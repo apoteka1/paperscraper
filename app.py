6b40b06e3d2d5391add88cfc81301d93cd6c4f7d
apoteka1 <joseph.ackroyd.1991@gmail.com>
2022-10-18 18:34:24 +0100

    change response format

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, jsonify, request
 from greq import get_counts
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def welcome():
     arg = request.args.get('search')
-    return jsonify(get_counts(arg))
+    res = {'searchTerm': arg, 'counts': get_counts(arg)}
+    return jsonify(res)
 
 
 if __name__ == '__main__':
