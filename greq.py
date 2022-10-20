47cdb7ddff1097f7076bb05d0f7e5556d50a5c31
Joe Ackroyd <joseph.ackroyd.1991@gmail.com>
2022-10-20 14:06:42 +0100

    add better site names

--- greq.py
+++ greq.py
@@ -1,20 +1,25 @@
+from itertools import count
 from bs4 import BeautifulSoup
 import grequests as greq
 import re
 
+
 def get_counts(search_term):
     search_term_re = r'\b(%s)\b' % search_term
-    sites = ["https://www.theguardian.com/uk", "https://www.dailymail.co.uk/home/index.html",
-             "https://www.bbc.co.uk/news", "https://www.thesun.co.uk/"]
+    sites = [("The Guardian", "https://www.theguardian.com/uk"), ("Daily Mail", "https://www.dailymail.co.uk/home/index.html"),
+             ("BBC", "https://www.bbc.co.uk/news"), ("The Sun", "https://www.thesun.co.uk/")]
+
     counts = {}
-    requests = (greq.get(link) for link in sites)
+    requests = (greq.get(site[1]) for site in sites)
     responses = greq.map(requests)
 
     for i, r in enumerate(responses):
         soup = BeautifulSoup(r.content, 'html.parser')
         mentions = soup.find_all(string=re.compile(
             search_term_re, re.IGNORECASE))
-        site_name = sites[i].split('www.')[1].split('.')[0]
+        site_name = sites[i][0]
         counts[site_name] = len(mentions)
 
     return counts
+
+
